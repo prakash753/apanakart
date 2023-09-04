ddac6ff8b285f8e4ef4aff9d5e58399668e75c07
maheshbyale <maheshbbyale@gmail.com>
2023-09-04 12:06:18 +0530

    added new images

---
+++ logcred/signup.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('signup-form');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const name = document.getElementById('uesr id').value;
+        const phone = document.getElementById('phone').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('confirm-password').value;
+
+        if (password !== confirmPassword) {
+            alert('Passwords do not match');
+        } else {
+            // Here you can perform further actions like sending the data to a server
+            // or storing it in local storage.
+            alert('Signup successful');
+        }
+    });
+});
